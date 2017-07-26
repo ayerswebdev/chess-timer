b35bef1690f023116cca6bc3d5ad256b4cbd10e7
Jacob Ayers <ayerswebdev@gmail.com>
2017-07-25 19:06:58 -0700

    Completed timer functions
    Timers are now fully operational. Next step is building an interface that actually looks good!

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
     var blackTimer = new Timer($("#time-setter").val(), $("#black-countdown"));
     $(".landing").fadeToggle();
     whiteTimer.start();
+
+    addGameClickFunctions(whiteTimer, blackTimer);
   });
 });
 
@@ -11,13 +13,63 @@ function Timer(time, el) {
   el.text(time + ":00");
 
   var milli = time * 60000;
-  console.log(time + " minutes = " + milli + " milliseconds");
+  //console.log(time + " minutes = " + milli + " milliseconds");
 
-  var startTime, endTime, timePaused, timeResumed;
+  var m = time;
+  var s = 0;
+
+  var startTime, now, endTime, timePaused, timeResumed, int;
+
+  this.hasBegun = false;
 
   this.start = function() {
-    startTime = new Date().getTime();
-    endTime = startTime + milli;
-    console.log(startTime + " is current time; " + endTime + " is ending time");
+    this.hasBegun = true;
+    setTimeout(function() {
+      startTime = new Date().getTime();
+      endTime = startTime + milli;
+
+      int = setInterval(function() {
+        now = new Date().getTime();
+        m = Math.floor((endTime - now) / 60000);
+        s = Math.floor(((endTime - now) / 1000) % 60);
+        s = s < 10 ? "0" + s : s;
+
+        el.text(m + ":" + s);
+        //console.log(m + ":" + s + " remain");
+      }, 200);
+    }, 750);
   };
+
+  this.pause = function() {
+    timePaused = new Date().getTime();
+    //console.log("Paused at " + timePaused);
+    clearInterval(int);
+  };
+
+  this.resume = function() {
+    timeResumed = new Date().getTime();
+    endTime += (timeResumed - timePaused);
+
+    int = setInterval(function() {
+      now = new Date().getTime();
+      m = Math.floor((endTime - now) / 60000);
+      s = Math.floor(((endTime - now) / 1000) % 60);
+      s = s < 10 ? "0" + s : s;
+
+      el.text(m + ":" + s);
+      //console.log(m + ":" + s + " remain");
+    }, 200);
+  };
+}
+
+function addGameClickFunctions(white, black) {
+  $("#pause-white").click(function() {
+    white.pause();
+    !black.hasBegun ? black.start() : black.resume();
+  });
+
+  $("#pause-black").click(function() {
+    black.pause();
+    white.resume();
+  });
 }
